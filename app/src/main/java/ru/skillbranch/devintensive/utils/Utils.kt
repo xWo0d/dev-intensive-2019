cd4d7033b8042a463d89a955df9d3958dd7e655c
Victor <vdor72@gmail.com>
2019-07-10 20:43:06 +0300

    fixed parseFullName

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -2,12 +2,15 @@ package ru.skillbranch.devintensive.utils
 
 object Utils {
     fun parseFullName(fullName: String?): Pair<String?, String?> {
-        val parts: List<String>? = fullName?.split(" ")
 
-        val firstName = parts?.getOrNull(0)
-        val lastName = parts?.getOrNull(1)
+        fullName?.takeIf { !it.isBlank() }?.let {
+            val parts: List<String>? = fullName?.split(" ")
+            val firstName = parts?.getOrNull(0)
+            val lastName = parts?.getOrNull(1)
+            return firstName to lastName
+        }
 
-        return firstName to lastName
+        return null to null
     }
 
     fun transliteration(payload: String, divider: String = " "): String {

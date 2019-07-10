6713f50e601b210945bff25b070956804fc39013
Victor <vdor72@gmail.com>
2019-07-10 19:36:59 +0300

    hometask 2 initial commit

---
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -0,0 +1,20 @@
+package ru.skillbranch.devintensive.utils
+
+object Utils {
+    fun parseFullName(fullName: String?): Pair<String?, String?> {
+        val parts: List<String>? = fullName?.split(" ")
+
+        val firstName = parts?.getOrNull(0)
+        val lastName = parts?.getOrNull(1)
+
+        return firstName to lastName
+    }
+
+    fun transliteration(payload: String, divider: String = " "): String {
+        return payload
+    }
+
+    fun toInitials(firstName: String?, lastName: String?): String? {
+        return "$firstName $lastName"
+    }
+}
